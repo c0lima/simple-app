787b6f139efcad8e762195bb1b716e93720a1101
c0lima <colima.msc@gmail.com>
2023-06-07 00:37:22 -0400

    update autostart cluster

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+
+
+#function is_es_cluster_healthy() {
+#  HEALTH_STATUS=$(curl -s "${ES_SERVICE_URL}/_cluster/health" | jq -r '.status')
+#  if [[ $HEALTH_STATUS == "green" ]] || [[ $HEALTH_STATUS == "yellow" ]]; then
+#    return 0
+#  else
+#    return 1
+#  fi
+#}
+
 # Create a directory on your host machine to store Elasticsearch data
 mkdir -p ~/elasticsearch-data
 
 # Start Minikube
 minikube start --extra-config="apiserver.enable-admission-plugins=DefaultStorageClass" --extra-config="apiserver.authorization-mode=Node,RBAC"
+
 # Stop until Minikube is ready
 while [[ $(minikube status -o json | jq -r '.Host') != "Running" ]]; do
   echo "Waiting for Minikube to be ready..."
@@ -30,29 +42,43 @@ helm install keda kedacore/keda -n myapp
 # Apply your Kubernetes configuration
 kubectl apply --recursive -f k8s/base/.
 
-# Wait for Elasticsearch pod(s) to be running
-ES_PODS_RUNNING=false
-while [ $ES_PODS_RUNNING == false ]; do
-  ES_PODS_STATUS=$(kubectl get pods -n myapp -l app=elasticsearch -o jsonpath='{.items[*].status.phase}')
-  if [[ $ES_PODS_STATUS == "Running" ]]; then
-    ES_PODS_RUNNING=true
-  else
-    echo "Waiting for Elasticsearch pods to be ready..."
-    sleep 10
-  fi
-done
-
-# Get the name of one of the Elasticsearch pods
-ES_POD_NAME=$(kubectl get pods -n myapp -l app=elasticsearch -o jsonpath='{.items[0].metadata.name}')
-
-# Forward a local port (e.g., 9200) to the Elasticsearch service port
-kubectl port-forward -n myapp "$ES_POD_NAME" 9200:9200 &
-
-# Wait for port-forwarding to be established
-sleep 5
-
-# Update the Elasticsearch service URL
-ES_SERVICE_URL="http://localhost:9200"
-
-# Send the request to Elasticsearch
-curl -X POST "${ES_SERVICE_URL}/_scripts/nginx_requests_keda" -H 'Content-Type: application/json' --data-binary "@k8s/elasticsearch/nginx_requests_keda.json"
+## Wait for Elasticsearch pod(s) to be running
+#ES_PODS_RUNNING=false
+#while [ $ES_PODS_RUNNING == false ]; do
+#  ES_PODS_STATUS=$(kubectl get pods -n myapp -l app=elasticsearch -o jsonpath='{.items[*].status.phase}')
+#  if [[ $ES_PODS_STATUS == "Running" ]]; then
+#    ES_PODS_RUNNING=true
+#  else
+#    echo "Waiting for Elasticsearch pods to be ready..."
+#    sleep 10
+#  fi
+#done
+#
+## Get the name of one of the Elasticsearch pods
+#ES_POD_NAME=$(kubectl get pods -n myapp -l app=elasticsearch -o jsonpath='{.items[0].metadata.name}')
+#
+## Forward a local port (e.g., 9200) to the Elasticsearch service port
+#kubectl port-forward -n myapp "$ES_POD_NAME" 9200:9200 &
+#
+## Wait for port-forwarding to be established
+#sleep 5
+#
+## Update the Elasticsearch service URL
+#ES_SERVICE_URL="http://localhost:9200"
+#
+## Wait for Elasticsearch cluster to be healthy
+#ES_HEALTHY=false
+#while [ $ES_HEALTHY == false ]; do
+#  if is_es_cluster_healthy; then
+#    ES_HEALTHY=true
+#  else
+#    echo "Waiting for Elasticsearch cluster to be healthy..."
+#    sleep 10
+#  fi
+#done
+#
+## TODO: Automate this part
+#
+## We need to manually send this request to Elasticsearch
+## in order to upload the template to ES.
+##curl -X POST "${ES_SERVICE_URL}/_scripts/nginx_requests_keda" -H 'Content-Type: application/json' --data-binary "@k8s/elasticsearch/nginx_requests_keda.json"
